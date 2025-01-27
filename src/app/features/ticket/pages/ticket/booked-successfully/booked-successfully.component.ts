7c7243d098dc67b70fcfb2b6339e33df3de928ad
HONEY\nick5 <genghao2021@gmail.com>
2025-01-27 23:26:01 +0800

    新增訂位成功頁面

---
+++ src/app/features/ticket/pages/ticket/booked-successfully/booked-successfully.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booked-successfully',
+  standalone: true,
+  imports: [],
+  templateUrl: './booked-successfully.component.html',
+  styleUrl: './booked-successfully.component.scss'
+})
+export class BookedSuccessfullyComponent {
+
+}
