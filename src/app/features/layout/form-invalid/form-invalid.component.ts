1d28aeaeafcb2e0e311822907e4e207c8e298e3d
HONEY\nick5 <genghao2021@gmail.com>
2025-01-27 13:42:31 +0800

    新增回首頁

--- src/app/features/layout/form-invalid/form-invalid.component.ts
+++ src/app/features/layout/form-invalid/form-invalid.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
 import { CoreModule } from '../../../core/core.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-invalid',
@@ -10,4 +11,13 @@ import { CoreModule } from '../../../core/core.module';
   templateUrl: './form-invalid.component.html',
   styleUrl: './form-invalid.component.scss',
 })
-export class FormInvalidComponent {}
+export class FormInvalidComponent {
+  constructor(private router: Router) {}
+
+  /**
+   * 重導向回首頁
+   */
+  redirectHome() {
+    this.router.navigate(['/home']);
+  }
+}
