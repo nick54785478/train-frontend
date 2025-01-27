9810885e3f461a37bd8ecb9eed788a44c2abd4ce
HONEY\nick5 <genghao2021@gmail.com>
2025-01-27 12:03:45 +0800

    新增 form-invalid Component

---
+++ src/app/features/layout/form-invalid/form-invalid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-invalid',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-invalid.component.html',
+  styleUrl: './form-invalid.component.scss'
+})
+export class FormInvalidComponent {
+
+}
